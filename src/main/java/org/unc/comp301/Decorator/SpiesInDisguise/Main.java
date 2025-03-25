90586949158f2d45539da91b796f35415049a971
prairie <prg@cs.unc.edu>
2025-03-25 14:47:57 -0400

    More descriptive print output for Perry.

--- src/main/java/org/unc/comp301/Decorator/SpiesInDisguise/Main.java
+++ src/main/java/org/unc/comp301/Decorator/SpiesInDisguise/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     System.out.println(perry.getDescription());
 
+    System.out.println("Perry takes off his hat again");
     perry.putOnDisguise();
     System.out.println(perry.getDescription());
   }
